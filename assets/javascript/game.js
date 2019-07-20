0252c2c73b29ea7dddf57649ad504ee795c11789
Antonio de las Morenas <a.j.delasmorenas@gmail.com>
2019-07-19 22:48:49 -0500

    Added a bordered area that appears between the defender and the player character, also added a couple of object variables for holding the stats of the characters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,22 +7,38 @@ $(document).ready(function () {
 
     var enemyChoice = false;
 
+    var battleCommenced = false;
+
+    var edStats = { hitPoints: 100, attackPower: 12, counterAttackPower: 32 };
+
+    var mustangStats = { hitPoints: 120, attackPower: 8, counterAttackPower: 24 };
+
+    var armstrongStats = { hitPoints: 150, attackPower: 6, counterAttackPower: 16 };
+
+    var scarStats = { hitPoints: 180, attackPower: 4, counterAttackPower: 8 };
+
+    /*assignStats() {
+        if (yourCharacter = "ed") {
+            hitPoints = 
+        }
+    };*/
+
     function initiateGame(x) {
-        $("#playerCharacter").append("<h3 class='text-center text-white mt-4'>Your Character</h3>");
         $("#" + x + "Card").unwrap().removeClass("my-4").addClass("mb-4");
-        $("#playerCharacter").append($("#" + x + "Card"));
+        $("#playerCharacter").append("<h3 class='text-center text-danger mt-4'>Your Character</h3>").append($("#" + x + "Card"));
         $("#changingTitle").text("Enemies Available to Attack").removeClass("text-white").addClass("text-danger");
+        $("#battleMessages").addClass("bg-dark border border-danger rounded my-4").html("<h3 class='text-center text-white row justify-content-center align-self-center mx-auto'>SELECT AN ENEMY TO FIGHT BELOW...</h3>");
         startState = false;
         yourCharacter = x;
         enemyChoice = true;
     };
 
     function chooseEnemy(x) {
-        $("#defender").append("<h3 class='text-center text-danger mt-4'>Defender</h3>");
         $("#" + x + "Card").unwrap().removeClass("my-4").addClass("mb-4");
-        $("#defender").append($("#" + x + "Card"));
+        $("#defender").append("<h3 class='text-center text-danger mt-4'>Defender</h3>").append($("#" + x + "Card"));
         currentDefender = x;
         enemyChoice = false;
+        battleCommenced = true;
     };
 
     $("#edCard").click(function () {
@@ -30,7 +46,7 @@ $(document).ready(function () {
             initiateGame("ed");
         }
 
-        else if (enemyChoice) {
+        else if ((enemyChoice) && (yourCharacter !== "ed")) {
             chooseEnemy("ed");
         };
     });
@@ -40,7 +56,7 @@ $(document).ready(function () {
             initiateGame("mustang");
         }
 
-        else if (enemyChoice) {
+        else if ((enemyChoice) && (yourCharacter !== "mustang")) {
             chooseEnemy("mustang");
         };
     });
@@ -50,7 +66,7 @@ $(document).ready(function () {
             initiateGame("armstrong");
         }
 
-        else if (enemyChoice) {
+        else if ((enemyChoice) && (yourCharacter !== "armstrong")) {
             chooseEnemy("armstrong");
         };
     });
@@ -60,7 +76,7 @@ $(document).ready(function () {
             initiateGame("scar");
         }
 
-        else if (enemyChoice) {
+        else if ((enemyChoice) && (yourCharacter !== "scar")) {
             chooseEnemy("scar");
         };
     });
