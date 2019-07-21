33605a9ee96c84f5988f264975bdeda4107bf6a6
Antonio de las Morenas <a.j.delasmorenas@gmail.com>
2019-07-21 15:52:54 -0500

    Added in logic for fighting enemies and made the game automatically cycle through them, added lose conditions as well, plus made the ATTACK button become a RESTART button that reloads the page after game over

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,19 +3,52 @@ $(document).ready(function () {
 
     var yourCharacter = "";
 
+    var yourHitPoints = 0;
+
+    var yourBaseAttack = 0;
+
+    var yourCurrentAttack = 0;
+
     var currentDefender = "";
+
+    var defenderHitPoints = 0;
+
+    var defenderCounterPower = 0;
 
     var enemyChoice = false;
 
-    var battleCommenced = false;
+    var noClickingAllowed = false;
 
-    var ed = { name: "Edward", hitPoints: 100, attackPower: 12, counterAttackPower: 32 };
+    var canClickRestart = false;
 
-    var mustang = { name: "Mustang", hitPoints: 120, attackPower: 8, counterAttackPower: 24 };
+    var characterNames = { ed: "Edward", mustang: "Mustang", armstrong: "Armstrong", scar: "Scar" };
 
-    var armstrong = { name: "Armstrong", hitPoints: 150, attackPower: 6, counterAttackPower: 16 };
+    function assignPlayerStats() {
+        if (yourCharacter === "ed") {
+            yourHitPoints = 100;
+            yourBaseAttack = 12;
+            yourCurrentAttack = 12;
+        }
 
-    var scar = { name: "Scar", hitPoints: 180, attackPower: 4, counterAttackPower: 8 };
+        else if (yourCharacter === "mustang") {
+            yourHitPoints = 120;
+            yourBaseAttack = 8;
+            yourCurrentAttack = 8;
+        }
+
+        else if (yourCharacter === "armstrong") {
+            yourHitPoints = 150;
+            yourBaseAttack = 6;
+            yourCurrentAttack = 6;
+        }
+
+        else {
+            yourHitPoints = 180;
+            yourBaseAttack = 4;
+            yourCurrentAttack = 4;
+        };
+    };
+
 
     function initiateGame(x) {
         $("#" + x + "Card").unwrap().removeClass("my-4").addClass("mb-4");
@@ -26,6 +59,29 @@ $(document).ready(function () {
         startState = false;
         yourCharacter = x;
         enemyChoice = true;
+        assignPlayerStats();
+    };
+
+    function assignDefenderStats() {
+        if (currentDefender === "ed") {
+            defenderHitPoints = 100;
+            defenderCounterPower = 32;
+        }
+
+        else if (currentDefender === "mustang") {
+            defenderHitPoints = 120;
+            defenderCounterPower = 24;
+        }
+
+        else if (currentDefender === "armstrong") {
+            defenderHitPoints = 150;
+            defenderCounterPower = 16;
+        }
+
+        else {
+            defenderHitPoints = 180;
+            defenderCounterPower = 8;
+        };
     };
 
     function chooseEnemy(x) {
@@ -33,17 +89,59 @@ $(document).ready(function () {
         $("#defender").append("<h3 class='text-center text-danger mt-4 mr-4'>Defender</h3>").append($("#" + x + "Card"));
         currentDefender = x;
         enemyChoice = false;
-        battleCommenced = true;
-        $("#battleColumn").removeClass("d-flex");//bookmark -->
+        $("#battleColumn").removeClass("d-flex");
         $("#battleMessages").empty().removeClass("d-flex").append("<h3 class='text-white text-center py-5 my-5'>Click on the ATTACK button to attack.</h3>");
         $("#buttonRow").removeClass("d-none").addClass("mt-4 pt-3");
         $("#attackButton").addClass("pl-0").append("<button type='button' id='attackDefender' class='btn btn-danger border border-white' style='width: calc(100% - 15px); height: 100px; font-family: chunkfive_printregular; font-size: 2.5vw;'>ATTACK</button>");
+        assignDefenderStats();
     };
 
-    $("#attackButton").on("click", "#attackDefender", function() {
-        alert("Oh wow! You attack " + currentDefender.toUpperCase() + "!")
-        $("#battleMessages").empty().append("<h3 class='text-white text-center py-5 my-5'>You deal " + eval() + " damage to " + window[armstrong].name + "!</h3>");
-    });//THIS IS A PROBLEM
+    function defenderDefeated() {
+        $("#defender").empty();
+        $("#attackButton").removeClass("pl-0").empty();
+        $("#battleColumn").addClass("d-flex");
+        $("#battleRow").removeClass("d-none").addClass("w-100");
+        $("#battleMessages").addClass("bg-dark border border-danger rounded my-4 d-flex").html("<h3 class='text-white align-self-center text-center mx-auto'>Select an enemy to fight below.</h3>");
+        noClickingAllowed = false;
+        enemyChoice = true;
+    };
+
+    function gameOver() {
+        $("#battleMessages").empty().append("<h3 class='text-white text-center pt-5 my-5'>You have been defeated!</h3>").append("<h3 class='text-white text-center pb-5 mb-5'>GAME OVER</h3>");
+        noClickingAllowed = true;
+        setTimeout( function() {
+            $("#attackDefender").text("RESTART");
+            canClickRestart = true;
+        }, 3000);
+    };
+
+    $("#attackButton").on("click", "#attackDefender", function () {
+        if (!noClickingAllowed) {
+            defenderHitPoints = defenderHitPoints - yourCurrentAttack;
+            $("#" + currentDefender + "HP").text("HP: " + defenderHitPoints);
+            $("#battleMessages").empty().append("<h3 class='text-white text-center py-5 my-5'>You attack " + characterNames[currentDefender] + " for " + yourCurrentAttack + " damage!</h3>");
+            yourCurrentAttack = yourCurrentAttack + yourBaseAttack;
+
+            if (defenderHitPoints > 0) {
+                yourHitPoints = yourHitPoints - defenderCounterPower;
+                $("#" + yourCharacter + "HP").text("HP: " + yourHitPoints);
+                $("#battleMessages").append("<h3 class='text-white text-center pb-5 mb-5'>" + characterNames[currentDefender] + " counters your attack for " + defenderCounterPower + " damage!</h3>");
+                if (yourHitPoints < 1) {
+                    gameOver();
+                };
+            }
+
+            else {
+                noClickingAllowed = true;
+                $("#battleMessages").empty().append("<h3 class='text-white text-center py-5 my-5'>You defeated " + characterNames[currentDefender] + "!</h3>");
+                setTimeout(defenderDefeated, 3000);
+            };
+        }
+
+        else if (canClickRestart) {
+            location.reload();
+        };
+    });
 
     $("#edCard").click(function () {
         if (startState) {
